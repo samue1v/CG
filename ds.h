6623128db0d7bc4edf92b362eb1734b4ebbfa42a
samue1v <samue1v@github.com>
2022-08-28 18:22:09 -0300

    Init of cg project

---
+++ ds.h
@@ -0,0 +1,47 @@
+#ifndef DataStructHeader
+#define DataStructHeader
+
+struct coordinate
+{
+    int x;
+    int y;
+    int z;
+
+};
+
+class Matrix{
+    public:
+        Matrix(int lines, int columns);
+        bool setVal(int x, int y,int val);
+        int getVal(int x, int y);
+
+    private:
+        int nLines;
+        int nColumns;
+        int ** matrix;
+
+};
+
+Matrix::Matrix(int lines, int columns){
+    this->nLines = lines;
+    this->nColumns = columns;
+    int ** m[lines];
+    this->matrix = *m;
+    for(int i =0;i<lines;i++){
+        int c[columns];
+        int *l = c;
+        this->matrix[i] = l;
+    }
+}
+
+int Matrix::getVal(int x, int y){
+    return (this->matrix)[x][y];
+}
+
+bool Matrix::setVal(int x, int y, int val){
+    if(val>=0){(this->matrix)[x][y] = val;return true;}
+    return false;  
+}
+
+#endif
+
