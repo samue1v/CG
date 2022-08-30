cf2e6855cb34e6408204e1e03a7ad945b48800c2
samue1v <samuelvfarias@hotmail.com>
2022-08-30 00:34:50 -0300

    minor fix

--- viewport.cpp
+++ viewport.cpp
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include "ds.h"
 #include "polygons.cpp"
+#include "Scene.cpp"
 
 class Viewport : Matrix{
   public:
-    Color TraceRay(Scene scene, coordinate O, Matrix D, int t_min, int t_max);
+    Coordinate canvasToViewport(int x, int y);
+    Color TraceRay(Scene scene, Coordinate O, Matrix D, int t_min, int t_max);
   private:
 
 };
 
 
-Color Viewport::TraceRay(Scene scene, coordinate O, Matrix D, int t_min, int t_max){
+Color Viewport::TraceRay(Scene scene, Coordinate O, Matrix D, int t_min, int t_max){
   long unsigned int closest_t = INF;
   Polygon closest_poly;
   
-  for (Polygon polygon : scene.elements){
+  for (Polygon polygon : scene.getPolygonArray(){
     //intersectRay tem que ser o método do poligono específico!
     long int t1, t2 = IntersectRay
   }
