e720862403fb2ce845397db0c1b72b0e05b64c4c
samue1v <samue1v@github.com>
2022-09-10 01:43:59 -0300

    Push test

---
+++ Object.h
@@ -0,0 +1,24 @@
+#ifndef OBJECTS_HEADER
+#define OBJECTS_HEADER
+#include <stdio.h>
+#include "Shapes.h"
+class Object{
+    public:
+        Object();
+        Object(char *name,int numberOfShapes);
+        int getShapeCount();
+        char * getName();
+        bool setName(char * newName);
+        bool setShape(Shape3D * shape);
+        bool setShapeAt(Shape3D * shape,int index);
+        Shape3D * getShapeAt(int index);
+
+
+    private:
+        int shapeNumMax; 
+        int shapeCount;
+        Shape3D ** shapes;
+        char *name;
+
+};
+#endif
