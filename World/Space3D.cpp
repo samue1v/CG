633ad5715890c66034a380211f31830e894d3e06
samue1v <samuelvfarias@hotmail.com>
2022-10-14 12:21:57 -0300

    Fixed makefile, now iit only recompiles when its necessary

--- World/Space3D.cpp
+++ World/Space3D.cpp
@@ -1,10 +1,12 @@
 
 #include "../DataStructures/Coordinate.h"
+/*
 #include "../DataStructures/Vector.h"
 #include "../DataStructures/Pair.h"
 #include "../DataStructures/Coordinate.h"
 #include "../Object_/Shapes.h"
 #include "Scene.h"
+*/
 #include "Space3D.h"
 
 #include <stdio.h>
